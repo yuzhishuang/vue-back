661a12b3e7c8a89be505bf98b8e9376c232630f8
yuzhishuang <yuzhishuang@100tal.com>
2019-09-26 16:07:50 +0800

    111

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-let http = require('http');
+let http = require('http')
 let users = [{id:111,name:'zhufeng1'},{id:222,name:'zhufeng2'},{id:333,name:'zhufeng3'}];
 let server = http.createServer(function(req,res){
   console.log(req.method,req.url);
