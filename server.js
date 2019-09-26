030b3e456b58adcc0ee04cda0a7dd79e95568d4a
yuzhishuang <yuzhishuang@100tal.com>
2019-09-26 21:07:32 +0800

    111

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ let http = require('http')
 let users = [{id:11,name:'yzs'},{id:2,name:'yzs'},{id:3,name:'yzs'}];
 let server = http.createServer(function(req,res){
   console.log(req.method,req.url);
-  if(req.url == '/api/users'){
+  if(req.url == '/users'){
     res.setHeader('Access-Control-Allow-Origin','*');
     res.end(JSON.stringify(users));
   }else{
