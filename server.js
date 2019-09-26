02fd338b21a422173f77c865429a31881144fd22
yuzhishuang <yuzhishuang@100tal.com>
2019-09-26 18:46:58 +0800

    1

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 let http = require('http')
-let users = [{id:11,name:'yzs'},{id:2,name:'yzs'},{id:3,name:'yzs'}];
+let users = [{id:1,name:'yzs'},{id:2,name:'yzs'},{id:3,name:'yzs'}];
 let server = http.createServer(function(req,res){
   console.log(req.method,req.url);
   if(req.url == '/api/users'){
